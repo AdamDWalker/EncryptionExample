4ecac32790ab74943cde646a63412cd05afdffe0
AdamDWalker <adamwalker03@googlemail.com>
2017-12-10 17:22:00 +0000

    Database connection works
    - Connecting to the database works
    - Simple error handling implemented

--- EncryptionExample.php
+++ EncryptionExample.php
@@ -7,6 +7,10 @@
 
   // Validate the information
 
+  $servername = "localhost";
+  $username = "root";
+  $password = ""; // Super secure - I know
+
   $submitted_info = array("name", "address", "postcode", "phonenumber", "mobilenumber");
 
   // Check the data is all there
@@ -19,8 +23,22 @@
     }
   }
 
-
   // Connect to database
+
+  try
+  {
+    $conn = new PDO("mysql:host=$servername;dbname=encryptiontest", $username, $password);
+
+    // Error mode set to Exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  }
+  catch (PDOException $e)
+  {
+    echo "Connection failed: " . $e->getMessage();
+  }
+
+
   // Encrypt Data
 
 
